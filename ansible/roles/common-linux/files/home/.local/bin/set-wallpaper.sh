be3f2abe003365d80ec35812abfbf2c16cae58cf
pfaivre <pierrem.faivre@free.fr>
2026-01-04 12:28:49 +0100

    Desktop updates and fixes - Disabled Hyprland animations - Use default cursor - Limit volume to 100% max - Fixed set-wallpaper.sh file selection - Removed Vim-plug and some packages

--- ansible/roles/common-linux/files/home/.local/bin/set-wallpaper.sh
+++ ansible/roles/common-linux/files/home/.local/bin/set-wallpaper.sh
@@ -8,20 +8,36 @@
 # $ set-wallpaper.sh next
 # $ set-wallpaper.sh
 
+DEBUG=0
+
 WALLPAPER_DIR="$HOME/.config/mydesktop/current-theme/wallpaper/"
 CURRENT_WALL=$(hyprctl hyprpaper listloaded)
 CURRENT_WALL_NAME=$(basename $CURRENT_WALL)
 
-mapfile -d '' -t AVAILABLE_WALL < <(find "$WALLPAPER_DIR" -name "wallpaper*" -type f,l -print0 | sort -z)
+if [[ DEBUG -eq 1 ]]; then
+    echo "DEBUG: CURRENT_WALL=$CURRENT_WALL"
+    echo "DEBUG: CURRENT_WALL_NAME=$CURRENT_WALL_NAME"
+fi
+
+mapfile -d '' -t AVAILABLE_WALL < <(find "$WALLPAPER_DIR" -type f,l -name "wallpaper*" -print0 | sort -k 2,2 -t. -u)
 AVAILABLE_WALL_COUNT=$(ls -1q $WALLPAPER_DIR/wallpaper* | wc -l)
 echo "Available wallpapers: $AVAILABLE_WALL_COUNT"
+if [[ DEBUG -eq 1 ]]; then
+    echo "DEBUG: AVAILABLE_WALL=$AVAILABLE_WALL"
+fi
 
 # Get the first wallpaper
 if [ $AVAILABLE_WALL_COUNT -eq 1 ] || [[ $1 = "first" ]]; then
-    WALLPAPER=$(find "$WALLPAPER_DIR" -type f,l -name "wallpaper*" | head -n 1)
+    if [[ DEBUG -eq 1 ]]; then
+        echo "DEBUG: Chose option \"first\""
+    fi
+    WALLPAPER=$(find "$WALLPAPER_DIR" -type f,l -name "wallpaper*" | sort -k 2,2 -t. -u | head -n 1)
 
 # Get the next wallpaper
 elif [[ $1 = "next" ]]; then
+    if [[ DEBUG -eq 1 ]]; then
+        echo "DEBUG: Chose option \"next\""
+    fi
     # Find current file index
     INDEX=-1
     for i in "${!AVAILABLE_WALL[@]}"; do
@@ -42,8 +58,15 @@ elif [[ $1 = "next" ]]; then
 
 # Get a random wallpaper that is not the current one
 else
+    if [[ DEBUG -eq 1 ]]; then
+        echo "DEBUG: Chose option \"random\""
+    fi
     WALLPAPER=$(find "$WALLPAPER_DIR" -type f,l -name "wallpaper*" ! -name "$CURRENT_WALL_NAME" | shuf -n 1)
 
+fi
+
+if [[ DEBUG -eq 1 ]]; then
+    echo "DEBUG: Chosen wallpaper: $WALLPAPER"
 fi
 
 # Apply the selected wallpaper, if found
