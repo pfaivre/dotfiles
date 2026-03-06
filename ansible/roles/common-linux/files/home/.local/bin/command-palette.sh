9ed3f85191144a1c665ded22c5d7414a3c485681
pfaivre <pierrem.faivre@free.fr>
2026-03-06 12:29:43 +0100

    Minor updates
    btop config update
    Switch back to the more generic cursor Adwaita
    Label updates for the command palette

--- ansible/roles/common-linux/files/home/.local/bin/command-palette.sh
+++ ansible/roles/common-linux/files/home/.local/bin/command-palette.sh
@@ -9,87 +9,88 @@ selected=$(pkill rofi; \
 
 case $selected in
   # Theme
-  "Theme: Summer Night")
+  "Theme → Summer Night")
     ~/.local/bin/set-theme.sh summer-night
     ;;
-  "Theme: Summer Day")
+  "Theme → Summer Day")
     ~/.local/bin/set-theme.sh summer-day
     ;;
-  "Theme: Cards Day")
+  "Theme → Cards Day")
     ~/.local/bin/set-theme.sh cards-day
     ;;
-  "Theme: Ocean Dark")
+  "Theme → Ocean Dark")
     ~/.local/bin/set-theme.sh ocean-dark
     ;;
-  "Theme: Bliss XP")
+  "Theme → Bliss XP")
     ~/.local/bin/set-theme.sh bliss-xp
     ;;
 
   # Power
-  "Power: Log-off")
+  "Power → Log-off")
     loginctl terminate-user $USER
     ;;
-  "Power: Sleep")
+  "Power → Sleep")
     systemctl suspend
     ;;
-  "Power: Restart")
+  "Power → Restart (reboot)")
     systemctl reboot
     ;;
-  "Power: Shutdown")
+  "Power → Shutdown (poweroff)")
     systemctl poweroff
     ;;
 
   # Display
-  "Display: Color temperature")
+  "Display → Color temperature")
     ~/.local/bin/set-screen-temperature.sh
     ;;
 
   # Utils
-  "Utils: Color picker")
+  "Utils → Color picker")
     sleep 0.1 # Give Rofi time to close
-    hyprpicker -a
+    color=$(hyprpicker -a)
+    notify-send -e -u low "Color picker" "Color copied to the clipboard: $color"
     ;;
-  "Utils: Screenshot (full screen)")
+  "Utils → Screenshot (full screen)")
     hyprshot -m output
     ;;
-  "Utils: Screenshot (region)")
+  "Utils → Screenshot (region)")
     hyprshot -m region
     ;;
-  "Utils: Screenshot (window)")
+  "Utils → Screenshot (window)")
     hyprshot -m window
     ;;
 
   # Personalization
-  "Personalization: Next wallpaper")
+  "Personalization → Next wallpaper")
     ~/.local/bin/set-wallpaper.sh next
     ;;
 
   # Setup
-  "Setup: Audio")
+  "Setup → Audio")
     alacritty --class Popup --command wiremix
     ;;
 
-  "Setup: Input")
+  "Setup → Input")
     alacritty --class Popup --command nvim ~/.config/hypr/input.conf
     ;;
 
-  "Setup: Keybindings")
+  "Setup → Keybindings")
     alacritty --class Popup --command nvim ~/.config/hypr/keybindings.conf
     ;;
 
-  "Setup: Monitors")
+  "Setup → Monitors")
     alacritty --class Popup --command nvim ~/.config/hypr/monitors.conf
     ;;
 
-  "Setup: Hypridle")
+  "Setup → Hypridle")
     alacritty --class Popup --command nvim ~/.config/hypr/hypridle.conf
     ;;
 
-  "Setup: Hyprsunset")
+  "Setup → Hyprsunset")
     alacritty --class Popup --command nvim ~/.config/hypr/hyprsunset.conf
     ;;
 
-  "System: Kill process")
+  "System → Kill process")
     ps -u $USER -o pid,comm | rofi -dmenu | awk "{print $1}" | xargs -r kill
     ;;
 
@@ -99,25 +100,25 @@ esac
 exit 0
 
 ### DATA ###
-Theme: Summer Night
-Theme: Summer Day
-Theme: Cards Day
-Theme: Ocean Dark
-Theme: Bliss XP
-Power: Log-off
-Power: Sleep
-Power: Restart
-Power: Shutdown
-Display: Color temperature
-Utils: Color picker
-Utils: Screenshot (full screen)
-Utils: Screenshot (region)
-Utils: Screenshot (window)
-Personalization: Next wallpaper
-Setup: Audio
-Setup: Input
-Setup: Keybindings
-Setup: Monitors
-Setup: Hypridle
-Setup: Hyprsunset
-System: Kill process
+Theme → Summer Night
+Theme → Summer Day
+Theme → Cards Day
+Theme → Ocean Dark
+Theme → Bliss XP
+Power → Log-off
+Power → Sleep
+Power → Restart (reboot)
+Power → Shutdown (poweroff)
+Display → Color temperature
+Utils → Color picker
+Utils → Screenshot (full screen)
+Utils → Screenshot (region)
+Utils → Screenshot (window)
+Personalization → Next wallpaper
+Setup → Audio
+Setup → Input
+Setup → Keybindings
+Setup → Monitors
+Setup → Hypridle
+Setup → Hyprsunset
+System → Kill process
